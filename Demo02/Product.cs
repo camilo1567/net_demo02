b723874a3e044f43cc4b74dd36b92f402a236fa7
CamiloHorta <camilo20113@hotmail.com>
2024-04-13 18:08:22 -0500

    diferentes formas de constructor

---
+++ Demo02/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary;
+
+public class Product(int id, string name, decimal unitPrice, int unitsInStock)
+{
+    public int Id => id;
+    public string Name => name;
+    public decimal UnitPrice => unitPrice;
+    public int UnitsInStock => unitsInStock;
+
+}
+
+
